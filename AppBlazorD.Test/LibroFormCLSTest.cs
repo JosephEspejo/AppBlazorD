16df70421fb42aa472bd6597630c67079dd19061
Alumno <Alumno@b405-09>
2025-08-12 16:18:48 -0400

    Agregar archivos de proyecto.

---
+++ AppBlazorD.Test/LibroFormCLSTest.cs
@@ -0,0 +1,47 @@
+using AppBlazor.Entities;
+using System.ComponentModel.DataAnnotations;
+using Xunit;
+
+namespace AppBlazorD.Test
+{
+    public class LibroFormCLSTest
+    {
+        private List<ValidationResult> validaModelo(object modelo)
+        {
+            var resultados = new List<ValidationResult>();
+            var contexto = new ValidationContext(modelo,null, null);
+            Validator.TryValidateObject(modelo, contexto, resultados, true);
+            return resultados;
+        }
+        [Fact]
+        public void ValidacionCamposVacios()
+        {
+            var libro = new LibroFormCLS();
+
+            var errores = validaModelo(libro);
+
+            Assert.Contains(errores, e => e.ErrorMessage!.Contains("El id es requerido") ||
+            e.ErrorMessage!.Contains("El valor debe ser un numero positivo"));
+
+            Assert.Contains(errores, e => e.ErrorMessage!.Contains("El titulo es requerido"));
+
+            Assert.Contains(errores, e => e.ErrorMessage!.Contains("El resumen es requerido"));
+
+        }
+
+        [Fact]
+        public void ValidacionCamposCorrectos()
+        {
+            var libro = new LibroFormCLS
+            {
+                idLibro = 1,
+                titulo = "Libro de prueba",
+                resumen = "Es un resumen del libro de pruebas"
+            };
+            
+            var errores = validaModelo(libro);
+
+            Assert.Empty(errores);
+        }
+    }
+}
